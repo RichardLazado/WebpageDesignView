13e0c2fbb1c6bd19f54a40e3560b1d77ef450f0f
richard lazado <58279596+RichardLazado@users.noreply.github.com>
2022-10-15 21:21:23 +0800

    Add files via upload

---
+++ MIDTERMPLANB/ExportDatatoExcel/ExportDatatoExcel/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmgenerate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(243, 15)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(168, 53)
+        Me.Button1.TabIndex = 33
+        Me.Button1.Text = "&Export Excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(44, 15)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(168, 53)
+        Me.Button2.TabIndex = 31
+        Me.Button2.Text = "&Get Data"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(432, 15)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(168, 53)
+        Me.Button7.TabIndex = 32
+        Me.Button7.Text = "&Reset"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(16, 94)
+        Me.DataGridView3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView3.MultiSelect = False
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.RowHeadersWidth = 51
+        Me.DataGridView3.Size = New System.Drawing.Size(648, 434)
+        Me.DataGridView3.TabIndex = 34
+        '
+        'frmgenerate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(679, 543)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.DataGridView3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "frmgenerate"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Export to Excel"
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView3 As System.Windows.Forms.DataGridView
+End Class
---
+++ MIDTERMPLANB/ExportDatatoExcel/ExportDatatoExcel/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Data.OleDb
+Public Class frmgenerate
+    Dim rdr As OleDbDataReader = Nothing
+    Dim dtable As DataTable
+    Dim con As OleDbConnection = Nothing
+    Dim adp As OleDbDataAdapter
+    Dim ds As DataSet
+    Dim cmd As OleDbCommand = Nothing
+    Dim dt As New DataTable
+    Dim cs As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\GenerallPayroll.accdb;Persist Security Info=False;"
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Try
+            con = New OleDbConnection(cs)
+            con.Open()
+            cmd = New OleDbCommand("select (EmployeeName) as [Employee Name], (AmountWithdrawn) as [Amount Withdrawn], (SCCC) as [SC], (OrigSalary) as [OR Salary], (NetPay) as [Net], (NameOfRecepient) as [Name of Recepient], (RTOB) as [Relationship to borrower], (Signature) as [Signature], (DueDate) as [Due Date], (RMKS) as [Remarks] from genpayfinal order by EmployeeName,DateofOrigin ", con)
+            Dim myDA As OleDbDataAdapter = New OleDbDataAdapter(cmd)
+
+            Dim myDataSet As DataSet = New DataSet()
+
+            myDA.Fill(myDataSet, "GenPayFinal")
+
+            DataGridView3.DataSource = myDataSet.Tables("GenPayFinal").DefaultView
+
+
+
+            con.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        DataGridView3.DataSource = Nothing
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If DataGridView3.RowCount = Nothing Then
+            MessageBox.Show("Sorry nothing to export into excel sheet.." & vbCrLf & "Please retrieve data in datagridview", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+        Dim rowsTotal, colsTotal As Short
+        Dim I, j, iC As Short
+        System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor
+        Dim xlApp As New Excel.Application
+
+        Try
+            Dim excelBook As Excel.Workbook = xlApp.Workbooks.Add
+            Dim excelWorksheet As Excel.Worksheet = CType(excelBook.Worksheets(1), Excel.Worksheet)
+            xlApp.Visible = True
+
+            rowsTotal = DataGridView3.RowCount - 1
+            colsTotal = DataGridView3.Columns.Count - 1
+            With excelWorksheet
+                .Cells.Select()
+                .Cells.Delete()
+                For iC = 0 To colsTotal
+                    .Cells(1, iC + 1).Value = DataGridView3.Columns(iC).HeaderText
+                Next
+                For I = 0 To rowsTotal - 1
+                    For j = 0 To colsTotal
+                        .Cells(I + 2, j + 1).value = DataGridView3.Rows(I).Cells(j).Value
+                    Next j
+                Next I
+                .Rows("1:1").Font.FontStyle = "Bold"
+                .Rows("1:1").Font.Size = 12
+                .Cells.Columns.AutoFit()
+                .Cells.Select()
+                .Cells.EntireColumn.AutoFit()
+                .Cells(1, 1).Select()
+
+            End With
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            'RELEASE ALLOACTED RESOURCES
+            System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default
+            xlApp = Nothing
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) 
+        Application.Exit()
+
+    End Sub
+End Class
